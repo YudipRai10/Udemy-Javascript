1cb67ac1ceef1bebdda209e8be76140abbcb7def
YudipRai10 <yudip11@gmail.com>
2025-05-20 09:26:28 +0545

    Constructor and Static

--- Constructor/app.js
+++ Constructor/app.js
@@ -1,78 +1,168 @@
-// class Person {
-//   constructor(name, age) {
+// // class Person {
+// //   constructor(name, age) {
+// //     this.name = name;
+// //     this.age = age;
+// //   }
+
+// //   greet() {
+// //     console.log(
+// //       "Hi, I am " + this.name + " and I am " + this.age + " years old"
+// //     );
+// //   }
+// // }
+
+// // const person = new Person("Max", 30);
+// // person.greet();
+
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// // Adding a method to the prototype of Person
+// Person.prototype.greet = function () {
+//   console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.`);
+// };
+
+// const person1 = new Person("Alice", 25);
+// const person2 = new Person("Bob", 30);
+
+// person1.greet();
+// person2.greet();
+
+// function Car(make, model) {
+//   this.make = make;
+//   this.model = model;
+// }
+
+// Car.prototype.startEngine = function () {
+//   console.log(`Starting the engine of ${this.make} ${this.model}`);
+// };
+
+// const car1 = new Car("Tesla", "Model 3");
+// const car2 = new Car("Ford", "Mustang");
+
+// car1.startEngine();
+// car2.startEngine();
+
+// class Animal {
+//   constructor(name) {
 //     this.name = name;
-//     this.age = age;
 //   }
 
-//   greet() {
-//     console.log(
-//       "Hi, I am " + this.name + " and I am " + this.age + " years old"
-//     );
+//   speak() {
+//     console.log(this.name + " makes a sound");
 //   }
 // }
 
-// const person = new Person("Max", 30);
-// person.greet();
+// class Dog extends Animal {
+//   constructor(name, breed) {
+//     super(name);
+//     this.breed = breed;
+//   }
 
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-}
+//   speak() {
+//     super.speak();
+//     console.log(this.name + " barks");
+//   }
+// }
 
-// Adding a method to the prototype of Person
-Person.prototype.greet = function () {
-  console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.`);
-};
+// const dog = new Dog("Rex", "Golden Retriever");
+// dog.speak();
 
-const person1 = new Person("Alice", 25);
-const person2 = new Person("Bob", 30);
+// function Hello(name) {
+//   this.name = name;
+// }
 
-person1.greet();
-person2.greet();
+// console.log(Hello.prototype);
+// console.log(Hello.__proto__);
 
-function Car(make, model) {
-  this.make = make;
-  this.model = model;
-}
+// function Car(make, model, year, color) {
+//   (this.make = make),
+//     (this.model = model),
+//     (this.year = year),
+//     (this.color = color);
+//   this.drive = function () {
+//     console.log(
+//       `You drive the ${this.color} ${this.make} ${this.model} made in year ${this.year}`
+//     );
+//   };
+// }
 
-Car.prototype.startEngine = function () {
-  console.log(`Starting the engine of ${this.make} ${this.model}`);
-};
+// const car1 = new Car("Ford", "Mustang", 2023, "red");
+// console.log(car1);
+// car1.drive();
 
-const car1 = new Car("Tesla", "Model 3");
-const car2 = new Car("Ford", "Mustang");
+// class Product {
+//   constructor(name, price) {
+//     this.name = name;
+//     this.price = price;
+//   }
 
-car1.startEngine();
-car2.startEngine();
+//   displayProduct() {
+//     console.log(`Product: ${this.name}`);
+//     console.log(`Price: $${this.price.toFixed(2)}`);
+//   }
 
-class Animal {
-  constructor(name) {
-    this.name = name;
+//   calculateTotal(salesTax) {
+//     return `$${(this.price + this.price * salesTax).toFixed(2)}`;
+//   }
+// }
+
+// const salesTax = 0.05;
+
+// const product1 = new Product("T-shirt", 19.99);
+// const product2 = new Product("Pant", 22.5);
+
+// product2.displayProduct();
+
+// const total1 = product1.calculateTotal(salesTax);
+// console.log("Total Price:", total1);
+
+// const total2 = product2.calculateTotal(salesTax);
+// console.log("Total Price:", total2);
+
+// class MathUtils {
+//   static PI = 3.14159;
+
+//   static getDiameter(diameter) {
+//     return diameter * 2;
+//   }
+
+//   static getCircumference(radius) {
+//     return 2 * this.PI * radius;
+//   }
+// }
+
+// console.log(MathUtils.PI);
+// console.log(MathUtils.getDiameter(20));
+// console.log(MathUtils.getCircumference(20));
+
+class User {
+  static userCount = 0;
+
+  constructor(username) {
+    this.username = username;
+    User.userCount++;
   }
 
-  speak() {
-    console.log(this.name + " makes a sound");
+  static getUserCount() {
+    console.log(`There are ${User.userCount} users online`);
+  }
+
+  sayHello() {
+    console.log(`Hello, my username is ${this.username}`);
   }
 }
 
-class Dog extends Animal {
-  constructor(name, breed) {
-    super(name);
-    this.breed = breed;
-  }
+const user1 = new User("Tom");
+const user2 = new User("Mat");
+const user3 = new User("Sam");
 
-  speak() {
-    super.speak();
-    console.log(this.name + " barks");
-  }
-}
+console.log(user1.username);
+user1.sayHello();
+user2.sayHello();
+user3.sayHello();
+// console.log(user1.userCount); not in instance but instead own by class
 
-const dog = new Dog("Rex", "Golden Retriever");
-dog.speak();
-
-function Hello(name) {
-  this.name = name;
-}
-
-console.log(Hello.prototype);
-console.log(Hello.__proto__);
+User.getUserCount();
